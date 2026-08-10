259b842e446c436655127d0897a842270af26e5c
foraitouse1 <foraitouse1@gmail.com>
2026-08-10 05:09:21 -0400

    Add files via upload

--- AI/PortableEngines/Chat-Text-VL/llama.cpp/IsolateEnv-Project.bat
+++ AI/PortableEngines/Chat-Text-VL/llama.cpp/IsolateEnv-Project.bat
@@ -28,31 +28,3 @@ goto DATA
 
 :DATA
 
-::added by 'DownloadInstall-CACert.bat'
-set "SSL_CERT_FILE=%COMMONPROGRAMFILES%\cacert.pem"
-ECHO SSL_CERT_FILE=%SSL_CERT_FILE%
-
-::added by 'Download-Llamacpp-Model.bat'
-set "_MODEL_DIR=%SCRIPT_DIR%Models"
-ECHO _MODEL_DIR=%_MODEL_DIR%
-
-::added by 'DownloadInstall-Llamacpp.bat'
-set "_LLAMA_BACKEND=vulkan"
-ECHO _LLAMA_BACKEND=%_LLAMA_BACKEND%
-
-::added by 'DownloadInstall-Llamacpp.bat'
-set "_LLAMA_DIR=%SCRIPT_DIR%llama.cpp\%_LLAMA_BACKEND%"
-ECHO _LLAMA_DIR=%_LLAMA_DIR%
-
-::added by 'DownloadInstall-Llamacpp.bat'
-set "LLAMA_CACHE=%SCRIPT_DIR%Models\llamacache"
-ECHO LLAMA_CACHE=%LLAMA_CACHE%
-
-::added by 'DownloadInstall-Llamacpp.bat'
-set "HF_HOME=%SCRIPT_DIR%Models\hfcache"
-ECHO HF_HOME=%HF_HOME%
-
-::added by 'Run-Llamacpp.bat'
-set "_PORT=11434"
-ECHO _PORT=%_PORT%
-
